cd0b71075d64ace4246860434a8b638ab1fbb018
Geetaprakash <86463646+Geetaprakash@users.noreply.github.com>
2022-02-03 15:49:08 +0530

    Pythonprograms

---
+++ FunctionPgmPY/distance.py
@@ -0,0 +1,15 @@
+
+import math
+class Distance:
+    @staticmethod
+    def euclidean_distance():
+        try:
+            x_point = int(input("Enter The Number For X-Point : ")) 
+            y_point = int(input("Enter The Number For Y-Point : "))
+            
+            distance_formula=math.sqrt(math.pow(x_point,2) + math.pow(y_point,2))#Formula To Calculate Euclidean Distance
+            print("Euclidean Distance From The Points (x,y) Is :",distance_formula)
+        except Exception as err:
+            print("** Error **", err) 
+            print("Enter Only Numbers")
+Distance.euclidean_distance() 
---
+++ PyhonPgm/Harmonic.py
@@ -0,0 +1,12 @@
+
+
+try:
+    number = int(input("Enter A Number: "))
+    assert number!=0
+except:
+    print("Number Is Zero")
+else:    
+    harmonic=0
+    for i in range(1,number+1):
+        harmonic=harmonic+1/i
+        print(harmonic)
